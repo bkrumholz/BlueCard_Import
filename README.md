47bdf8470fa0296b833b54e143fc7059afb5a460
Brian-Krumholz <Brian.Krumholz@ucsf.edu>
2023-09-15 11:00:19 -0700

    Merge versions for public use
    Merge build

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-# BlueCard_Import
-Python Script to take Blue Shield Prefix list and convert to an Epic CMG import
+This script converts the Blue Shield prefix file to a format that can be imported into Epic.
 
-California Blue Cross and Blue Shield use a three character prefix in their subscriber insurance numbers to indicate which payer should be billed for medical services. This sharing of liability program is called Blue Card.
+Assumptions:
+This import assumes that the prefixes will override the data in the first contact and that the contact date is 1/1/1900.
 
-For more on the Blue Card program, go to the following link:
-https://www.blueshieldca.com/bsca/bsc/wcm/connect/provider/provider_content_en/guidelines_resources/bluecard/faqs
+How to install:
+1) Download this repository to your computer. This can be done by cloning the repository from the Github Desktop application or you can enter the following from a Github command line:
 
-Blue Shield provides a list of prefixes with dates to indicate what payer destination should be used for each insurance prefix. This file is released on a monthly basis. This python program converts the Blue Shield list of prefixes to an Epic CMG Import format that contains only active prefixes for Blue Cross.
+git clone https://github.com/Brian-Krumholz/BlueCard_Import
 
-Example Blue Shield file format:
+2) Install the dependencies for the project. This can be done from python console with the following command:
+pip install -r requirements.txt
 
-Prefix|Occurrence in File|Destination (C=BC,T=BS)|Not used|Start Date|End Date
+How to use:
+1) Either execute the buildImport.py file from the IDE or type the following command into the command prompt:
+python buildImport.py
 
-A1A|01|C| |20190101|99999999|
-
-A1B|01|C| |20190101|99999999|
-
-A1C|01|C| |20180901|20191231|
-
-A1F|01|C| |20190101|99999999|
-
-ACB|01|T| |20200701|99999999|
-
-ACD|01|C| |20050501|20181231|
-
-ACD|02|C| |20190101|99999999|
+    *It is important to note that if you are running the program from the command prompt you should be in the project directory
+2) The first time it runs, a config.ini will be created. You should open up the config.ini file and update the settings as needed.
+3) Run the program a second time, it will now prompt for the import file. You only need to specify the filename and not the full path.
+4) The program will run and create an Epic import file in the output directory specified in config.ini.
+5) Transfer the import file to the Epic server.
+6) Open up chronicles for the CMP ini.
+7) Import the file using the CMP,1001 import specification
+8) Open up the CMP in record viewer or data courier and verify that it has the correct number of prefixes in it.
--- buildImport.py
+++ buildImport.py
@@ -1,43 +1,60 @@
 from datetime import date, datetime,timedelta, time
+from configparser import ConfigParser
+from os.path import exists
+##Make sure to update config.ini file with correct file locations and record ID
 
-def generateFile(input_file,output_dir,prefix_skip_list=[],cmg_id='973'):
+def generateFile(input_file,output_dir,cmp_id,prefix_skip_list=[],expire_lag=52):
     active_prefix_set = set([])
-    bs_skip = 0
-    dup_skip = 0
-    expire_skip = 0
-    list_skip = 0
+    list_stats={
+        "bs_skip": 0,
+        "dup_skip": 0,
+        "expire_skip": 0,
+        "list_skip": 0,
+        "total":0
+    }
     today = date.today()
-    expire_date = datetime.combine(today - timedelta(weeks=52), time(0, 0, 0, 0))  #Expire any prefix that was sunset more than 52 weeks ago.
+    expire_date = datetime.combine(today - timedelta(weeks=expire_lag), time(0, 0, 0, 0))  #Expire any prefix that was sunset more than 52 weeks ago.
     expire_date=expire_date.replace(day=1)
-    output_file = output_dir+"BC_import_" + expire_date.strftime('%Y%m%d')+"_"+today.strftime('%Y%m%d')+".txt"
+    output_file = output_dir+"BC_import_" + expire_date.strftime('%Y%m%d')+"_"+today.strftime('%Y%m%d')+".epicimport"
 
     print("Output File: ", output_file)
-
-    try:
-        opfile = open(output_file,'w')
-    except Exception as e:
-        print("Output file {0} could not be opened: {1}".format(output_file, e))
-        return None
     try:
         ipfile = open(input_file,"r")
     except Exception as e:
-        print("Input file {0} could not be opened: {1}".format_map(input_file, e))
+        print("\nInput file {0} could not be opened: {1}".format(input_file, e))
         return None
-    print("1,",cmg_id,file=opfile)   #Set import record to Blue Card component
-    print("20,1/1/1900",file=opfile) #Set contact to component contact
-    for line in ipfile:
+    try:
+        opfile = open(output_file,'w')
+    except Exception as e:
+        print("\nOutput file {0} could not be opened: {1}".format(output_file, e))
+        return None
+    if not cmp_id:
+        print("CMP ID was not specified")
+        exit(-1)
+    print("1,",cmp_id,file=opfile,sep='')   #Set import record to Blue Card component
+    print("20,1/1/1900",file=opfile,sep='') #Set contact to component contact
+    print("30,2",file=opfile,sep='') #Set component type to 'procedure'
+    for cnt,line in enumerate(ipfile):
         ok = False
         values = line.rstrip().split("|")
-        prefix = values[0]
-        type = values[2]
-        from_date = values[4]
-        to_date = values[5]
+        try:
+            prefix = values[0]
+            type = values[2]
+            from_date = values[4]
+            to_date = values[5]
+        except Exception as e:
+            print("Line is not in expected format of ""prefix|unused|type|from_date|to_date.")
+            print("Line {0}: {1}".format(cnt+1,line))
+            exit(-1)
+        list_stats['total']+=1
+        if not prefix or len(prefix) != 3:  #Skip lines if no 3 character prefix is present
+            continue
         if prefix in prefix_skip_list:
             print(prefix, type, from_date, to_date, " **SKIPPED - IGNORE LIST**", end="\n")
-            list_skip+=1
+            list_stats['list_skip']+=1
         if type != 'C':   #only look at prefixes for Blue Cross
             print(prefix, type, from_date, to_date, " **SKIPPED - BLUE SHIELD**", end="\n")
-            bs_skip+=1
+            list_stats['bs_skip']+=1
             continue
         if to_date == "99999999":    #handle unexpired prefixes
             to_date = "21991231"     #Good for the next century
@@ -46,27 +63,70 @@ def generateFile(input_file,output_dir,prefix_skip_list=[],cmg_id='973'):
         if ok:
             if prefix in active_prefix_set:
                 print(prefix, type, from_date, to_date, " **SKIPPED - ALREADY IN SET**", end="\n")
-                dup_skip+=1
+                list_stats['dup_skip']+=1
             else:
                 active_prefix_set.add(prefix)
                 print(prefix,type,from_date,to_date,end="\n")
-                print("1100",prefix, sep=",", file=opfile)
+                print("1100",prefix, sep=",", file=opfile)   #Output prefix to import file
         else:
             print(prefix, type, from_date, to_date," **SKIPPED - EXPIRED**", end="\n")
-            expire_skip
-    print("PREFIX COUNT: ",len(active_prefix_set))
+            list_stats['expire_skip']+=1
+    print_stats(len(active_prefix_set),list_stats)
     return output_file
 
 
-def print_stats(active_prefix_cnt,expire_skip):
-    pass
+def print_stats(active_prefix_cnt,list_stats):
+    print("")
+    print("Prefix Lines Reviewed: ",'%20s' % list_stats['total'])
+    print("Skipped Ignore List Count:",'%17s' % list_stats['list_skip'])
+    print("Skipped Blue Shield Prefix Count:", '%10s' % list_stats['bs_skip'])
+    print("Skipped Expired Prefix Count:", '%14s' % list_stats['expire_skip'])
+    print("Skipped Duplicate Prefix Count:", '%12s' % list_stats['dup_skip'])
+    print("Import Prefix Count: ", '%22s' % active_prefix_cnt)
+
+
+
+def createConfig() -> None:
+    config_object = ConfigParser()
+    config_object['Default'] = {
+    "input_dir": r'C:/Output/BlueCard/        #Directory where Blue Shield file is located',
+    "cmp_id": '973        #CMP record ID to use for import to Epic (Record will be overridden)',
+    "output_dir": r'C:/Output/BlueCard/        #Directory for output of import file',
+    "prefix_skip": '        #Comma delimited list of prefixes to skip over',
+    "expire_lag:": '52        #Number of weeks to continue using prefix after its expiration date'
+    }
+
+    with open('config.ini', 'w') as conf:
+        config_object.write(conf)
+    print('A new config.ini file has been created in the root directory of this script. Please update the file prior to running the script again.')
+    exit(-2)
+
+
+def readConfig(config='Default'):
+    if not exists('config.ini'):
+        print("Creating config.ini file")
+        createConfig()
+    config_object = ConfigParser(inline_comment_prefixes="#")
+    config_object.read("config.ini")
+    config_object = config_object[config]
+    return config_object
 
 if __name__=="__main__":
-    input_dir = r'C:/Output/BlueCard/'  #Hardcode directory for ease of use
-    cmg_id = '973'  #Hardcode component used to contain prefix list
+    config = readConfig('Default')
+    input_dir = config['input_dir']
+    if len(input_dir)>0 and input_dir[-1] != "/":
+        input_dir+="/"
+    cmp_id = config['cmp_id']
     input_file = input('Filename:')
+    if not input_file:
+        print("No input file specified.")
+        exit(0)
     input_fullpath_file=input_dir+input_file
-    output_dir = r'C:/Output/BlueCard/'
-    output_file=generateFile(input_fullpath_file,output_dir,cmg_id)
-    print("File sent to: ",output_file)
-
+    output_dir = config['output_dir']
+    if len(output_dir)>0 and output_dir[-1] != "/":
+        output_dir += "/"
+    output_file = generateFile(input_fullpath_file,output_dir,cmp_id)
+    if output_file:
+        print("Epic Import File Generated At: ",output_file)
+    else:
+        print("Epic Import File failed to generate.")
